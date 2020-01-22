7a42355316d0d2eb2b6b34e8b8474eb98e743ee2
ibraheem Hasan <rules656@gmail.com>
2020-01-22 12:58:06 +0000

    Automatically backed up by Learn

--- src/Cell.js
+++ src/Cell.js
@@ -22,7 +22,7 @@ export default class Cell extends Component {
       <div 
         className="cell"
         style={{backgroundColor: this.state.color}}
-        onClick={this.handleClick}>
+        onClick={e => this.handleClick()}>
       </div>
     )
   }
